08e7b41992232e17908b3f20d9f64fd794adf2dd
HelloWorld <104149332+ming-14@users.noreply.github.com>
2025-08-19 18:02:59 +0800

    Delete Tool directory

--- Tool/结束希沃白板进程.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-taskkill -f -im EasiUpdate3Protect.exe
-taskkill -f -im EasiUpdate3.exe
-taskkill -f -im EasiUpdate.exe
-taskkill -f -im EasiNote.exe
-taskkill -f -im EasiAgent.exe
-taskkill -f -im EasiNote.BrowserSubprocess.exe
